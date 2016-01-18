c08ebde09ced4393a72b53d9431c84222142e818
Joe Gibson <gibsjose@mail.gvsu.edu>
2016-01-18 01:14:29 -0500

    Updating zxcv to version 2.1.0

--- zxcv.rb
+++ zxcv.rb
@@ -1,9 +1,9 @@
 class Zxcv < Formula
   desc "Show password strength statistics using the zxcvbn library"
   homepage "https://github.com/gibsjose/zxcv"
-  url "https://github.com/gibsjose/zxcv/archive/2.0.0.tar.gz"
-  version "2.0.0"
-  sha256 "cb3c6cf0cea89885f314726aa00b746e409dcc15bdccaa00b5bada7798172186"
+  url "https://github.com/gibsjose/zxcv/archive/2.1.0.tar.gz"
+  version "2.1.0"
+  sha256 "5064c6f71bc4076fd1b4024e3df2b506f918e253c7c7e8a51a0ae50a8ae5c77a"
 
   def install
       bin.install "zxcv"
